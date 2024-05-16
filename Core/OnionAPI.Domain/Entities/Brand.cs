4e3fee87d3d8a79518c815aa6ad2d9bd3132e4e6
muhammedsalihkoz <salih40@outlook.com>
2024-05-16 13:19:15 +0300

    Add project files.

---
+++ Core/OnionAPI.Domain/Entities/Brand.cs
@@ -0,0 +1,22 @@
+﻿using OnionAPI.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionAPI.Domain.Entities
+{
+    public class Brand:BaseEntity
+    {
+        public Brand()
+        {
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+
+    }
+}
---
+++ Core/OnionAPI.Domain/Entities/Detail.cs
@@ -0,0 +1,26 @@
+﻿using OnionAPI.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionAPI.Domain.Entities
+{
+    public class Detail:BaseEntity
+    {
+        public Detail()
+        {
+        }
+        public Detail(string title,string description,int categoryId)
+        {
+            Title = title;
+            Description = description;
+            CategoryId = categoryId;
+        }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
